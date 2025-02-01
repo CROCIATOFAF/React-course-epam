7e75ce45df9872c9bebf340160852c340c9f4850
David Gogua <tortex0.60@gmail.com>
2025-02-01 23:49:08 +0400

    feat: add background animation

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -13,6 +13,10 @@ class App extends Component {
   render() {
     return (
       <div className="app-container">
+        <video className="video-background" autoPlay loop muted>
+          <source src="/earth.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <h1>Discover</h1>
         <span>our intergalactic multimedia collections</span>
         <Home />
