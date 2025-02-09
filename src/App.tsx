9542a150ed69d2849f5e7c3badbc5f5b7c6a10d7
David Gogua <tortex0.60@gmail.com>
2025-02-09 23:18:52 +0400

    feat: add styles to NotFound component and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,37 @@ const NotFound = lazy(() => import('./components/NotFound/NotFound'));
 
 const App: React.FC = () => {
   return (
-    <div className="app-container">
-      <video className="video-background" autoPlay muted loop>
-        <source src="earth.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="app-container">
+              <video className="video-background" autoPlay muted loop>
+                <source src="earth.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
 
-      <div className="app__text-container">
-        <h1>Discover</h1>
-        <span>our intergalactic multimedia collections</span>
-        <p className="app__text-container-important">
-          Enter: &apos;simulate404&apos; or &apos;simulate500&apos; to see error
-          messages, as NASA API will send back 200 regardless
-        </p>
-      </div>
+              <div className="app__text-container">
+                <h1>Discover</h1>
+                <span>our intergalactic multimedia collections</span>
+                <p className="app__text-container-important">
+                  Enter: &apos;simulate404&apos; or &apos;simulate500&apos; to
+                  see error messages, as NASA API will send back 200 regardless
+                </p>
+              </div>
 
-      <Suspense fallback={<Spinner />}>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={null} />
-            <Route path="details/:id" element={<DetailCard />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </div>
+              <MainLayout />
+            </div>
+          }
+        >
+          <Route index element={null} />
+          <Route path="details/:id" element={<DetailCard />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -4,6 +4,7 @@ import styles from './NotFound.module.css';
 const NotFound: React.FC = () => {
   return (
     <div className={styles.notFound}>
+      <img src="/error.svg" alt="error window image" />
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
