87ae85c6d63a092cd5db578e6080f5887c7912b4
David Gogua <tortex0.60@gmail.com>
2025-03-14 21:51:01 +0400

    Testing Husky

--- src/components/Spinner/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Spinner.module.css';
-
-const Spinner: React.FC = () => {
-  return <div data-testid="spinner" className={styles.spinner}></div>;
-};
-
-export default Spinner;
