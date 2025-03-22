685a0297912c3905b82f0423a455c39aad19d162
David Gogua <tortex0.60@gmail.com>
2025-03-22 19:32:46 +0400

    init: start performance task

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import UncontrolledFormPage from './pages/UncontrolledFormPage';
-import HookFormPage from './pages/HookFormPage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 
-const App: React.FC = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Router>
-      <nav style={{ marginBottom: '1rem' }}>
-        <Link to="/">Main</Link> |{' '}
-        <Link to="/uncontrolled">Uncontrolled Form</Link> |{' '}
-        <Link to="/hook-form">Hook Form</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/uncontrolled" element={<UncontrolledFormPage />} />
-        <Route path="/hook-form" element={<HookFormPage />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <a href="https://vite.dev">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
-};
+}
 
 export default App;
